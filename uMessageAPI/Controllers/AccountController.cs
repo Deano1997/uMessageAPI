96b95bfaacd2a6d39f0662932fedd9a0d91c7724
Dean Van Damme <dean.vandamme.y9427@student.hogent.be>
2019-04-08 02:07:27 +0200

    Added initial structure for account controller and visual code tasks.

---
+++ uMessageAPI/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace uMessageAPI.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase {
+
+        // TODO: Provide CreateToken endpoint
+
+    }
+}
