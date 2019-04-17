8ee5f730b87afbbc30bf5fcd5f2492085fc9e96e
Dean Van Damme <dean.vandamme.y9427@student.hogent.be>
2019-04-18 00:40:24 +0200

    fixtures / db tests

---
+++ uMessageApi.Tests/Fixtures/EntityFixture.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using uMessageAPI.Data;
+
+namespace uMessageApi.Tests.Fixtures {
+    class EntityFixtures {
+
+        private ApplicationDbContext context;
+
+        public ApplicationDbContext Context {
+            get {
+
+                if (context == null) {
+
+                    context = CreateDbContext();
+                }
+
+                return context;
+            }
+        }
+
+        protected ApplicationDbContext CreateDbContext() {
+            var options = new DbContextOptionsBuilder()
+              .UseInMemoryDatabase(databaseName: "uMessage")
+              .Options;
+
+            return new ApplicationDbContext(options);
+        }
+   
+    }
+}
