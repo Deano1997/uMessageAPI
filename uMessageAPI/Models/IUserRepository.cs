06cda1b51c595b34612a03181decbd0b4b10f666
Dean Van Damme <dean.vandamme.y9427@student.hogent.be>
2019-04-30 00:44:02 +0200

    UserRepository

--- uMessageAPI/Models/IUserRepository.cs
+++ uMessageAPI/Models/IUserRepository.cs
@@ -1,3 +1,28 @@
+using System.Collections.Generic;
+
 namespace uMessageAPI.Models {
-    public interface IUserRepository : Generics.IEntityRepository<User> { }
+    public interface IUserRepository : Generics.IEntityRepository<User> {
+
+        User FindById(string id);
+
+        IEnumerable<User> GetAll();
+
+        void Add(User user);
+        void AddWithPassword(User user,string password);
+
+        void Update(User user);
+
+        void saveChanges();
+
+
+    }
+
+    
+
+
+
+    
+
+
+
 }
